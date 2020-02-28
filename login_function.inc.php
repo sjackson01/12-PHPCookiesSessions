d347f8e5390e5f13c241068297b6d5903430ce71
sjackson01 <stevenpjackson0@gmail.com>
2020-02-28 11:16:07 -0600

    Add (login_function.inc.php>Hunk 1) : Create redirect_user() and check_login().

--- login_function.inc.php
+++ login_function.inc.php
@@ -23,7 +23,63 @@ function redirect_user($page = 'index.php'){
     // Redirect the user: 
     header("Location: $url");
     exit(); // Quit the script
-}
+
+} // End of redirect_user() function
+
+/* This function validates the form data (the email address and password)
+ * If both are present, the database is queried.
+ * The function requires a database connection.
+ * The funtion returns an array of information, including:
+ * - a TRUE/FALSE variable indicating success
+ * - an array of either errors or the database result
+ */
+
+function check_login($dbc, $email = '', $pass = ''){
+
+
+    $error = []; // Initialize the error array
+    
+    // Validate the email address:
+    if (empty($email)){
+        $errors[] =  'You forgot to enter your email address.';
+    }else{
+        $p = mysqli_real_escape_string($dbc, trim($email));
+    }
+
+    // Validate the password:
+    if (empty($pass)){
+        $errors[] = 'You forgot to enter your password.';
+    }else{
+        $p = mysqli_real_escape_string($dbc, trim($pass));
+    }
+
+    // If no  errors: 
+    if (empty($errors)){
+
+        //Retrieve the user_id and first_name for that email/password combination:
+        $q = "SELECT user_id, first_name
+              FROM users 
+              WHERE email='$e' 
+              AND pass=SHA2('$p', 512)";
+        $r = @mysqli_query($dbc, $q); // Run the query.
+        
+        // Check the result: 
+        if (mysqli_num_rows($r) == 1){
+
+            // Fetch the record:
+            $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+
+            // Return true and the record: 
+            return [true, $row];
+        }else{ // Not a match
+            $error[] = 'The email address and password entered to not match those on file.';
+        }
+    }// End of empty ($errors) IF
+
+    // Return false and the errors: 
+    return [false, $errors];
+    
+}  // End of check_login() function 
 
  
  
