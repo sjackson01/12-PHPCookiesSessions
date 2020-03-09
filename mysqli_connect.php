5704a63f12ec814d01fff59b9e2f491df2920bca
sjackson01 <stevenpjackson0@gmail.com>
2020-03-09 10:54:50 -0500

    Modify (mysqli_connect>Hunk 1) : Refactor file to use OOP. Modify (view_users.php>Hunk 1 - 3) : Refactor file to use OOP.

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,17 +1,53 @@
-<?php # Script 9.2 - mysqli_connect.php
-
-// This file contains the database access information.
+<?php # Script 16.1- mysqli_oop_connect.php
+// This file contains the databse access information. 
 // This file also establishes a connection to MySQL,
-// selects the database, and sets the encoding.
+// selects the databse, and sets the encoding. 
+// The MySQL interactions use OOP!
 
-// Set the database access information as constants:
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'tiger');
-define('DB_HOST', 'mysql:3306');
-define('DB_NAME', 'sitename');
+// Set the database access information
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PASSWORD', 'tiger');
+DEFINE('DB_HOST', 'mysql:3306');
+DEFINe('DB_NAME', 'sitename');
 
-// Make the connection:
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MySQL: ' . mysqli_connect_error() );
+// Make connection
+$mysqli = new MySQLi(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-// Set the encoding...
-mysqli_set_charset($dbc, 'utf8');
+// Verify the connection 
+if($mysqli->connect_error) {
+    echo $mysqli->connect_error;
+}else{ // Establish encoding. 
+    $mysqli->set_charset('utf8');
+}
+
+/*
+// Use print are to debug objects in PHP code 
+echo '<pre>' . print_r($mysqli, 1) . '</pre>';
+
+
+mysqli Object
+(
+    [affected_rows] => 0
+    [client_info] => mysqlnd 5.0.12-dev - 20150407 - $Id: 3591daad22de08524295e1bd073aceeff11e6579 $
+    [client_version] => 50012
+    [connect_errno] => 0
+    [connect_error] => 
+    [errno] => 0
+    [error] => 
+    [error_list] => Array
+        (
+        )
+
+    [field_count] => 0
+    [host_info] => mysql:3306 via TCP/IP
+    [info] => 
+    [insert_id] => 0
+    [server_info] => 8.0.19
+    [server_version] => 80019
+    [stat] => Uptime: 44227  Threads: 2  Questions: 76  Slow queries: 0  Opens: 164  Flush tables: 3  Open tables: 84  Queries per second avg: 0.001
+    [sqlstate] => 00000
+    [protocol_version] => 10
+    [thread_id] => 27
+    [warning_count] => 1
+)
+*/ 
--- view_users.php
+++ view_users.php
@@ -13,10 +13,10 @@ require('mysqli_connect.php'); // Connect to the db.
 
 // Make the query:
 $q = "SELECT CONCAT(last_name, ', ', first_name) AS name, DATE_FORMAT(registration_date, '%M %d, %Y') AS dr FROM users ORDER BY registration_date ASC";
-$r = @mysqli_query($dbc, $q); // Run the query.
+$r = $mysqli->query($q); // Run the query.
 
 // Count the number of returned rows:
-$num = mysqli_num_rows($r);
+$num = $r->num_rows;
 
 if ($num > 0) { // If it ran OK, display the records.
 
@@ -35,14 +35,14 @@ if ($num > 0) { // If it ran OK, display the records.
 ';
 
 	// Fetch and print all the records:
-	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-		echo '<tr><td align="left">' . $row['name'] . '</td><td align="left">' . $row['dr'] . '</td></tr>
-		';
+	while ($row = $r->fetch_object()) {
+		echo '<tr><td align="left">' . $row->name . '</td><td align="left">' . $row->dr . '</td></tr>';
 	}
 
 	echo '</tbody></table>'; // Close the table.
 
-	mysqli_free_result ($r); // Free up the resources.
+	$r->free(); // Free up the resources
+	unset($r); 
 
 } else { // If no records were returned.
 
@@ -50,7 +50,8 @@ if ($num > 0) { // If it ran OK, display the records.
 
 }
 
-mysqli_close($dbc); // Close the database connection.
+$mysqli->close();
+unset($mysqli);
 
 include('inc/footer.php');
 ?>
